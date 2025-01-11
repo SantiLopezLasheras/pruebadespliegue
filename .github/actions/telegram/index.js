8017e54b0a960f792463489efdb448fdd7aa20c1
santijll <santijll@example.com>
2025-01-12 00:42:46 +0100

    ptra vez

--- .github/actions/telegram/index.js
+++ .github/actions/telegram/index.js
@@ -5,6 +5,7 @@ const token = core.getInput("telegram_token");
 const ChatID = core.getInput("telegram_id_user");
 
 // resultados de los jobs
+const resultadoBadge = core.getInput("resultado_badge");
 const resultadoCypress = core.getInput("resultado_cypress");
 
 const bot = new TelegramBot(token, { polling: true });
@@ -14,7 +15,7 @@ try {
   S'ha realitzat un push en la branca main que ha provocat l'execució del workflow nom_repositori_workflow amb els següents resultats:\n
   - linter_job: resultat associat\n
   - cypress_job: ${resultadoCypress}\n
-  - add_badge_job: resultat associat\n
+  - add_badge_job: ${resultadoBadge}\n
   - deploy_job: resultat associat\n`;
 
   bot.sendMessage(ChatID, message);
