5d19660ecf150ff7a10a6e6b50e3fbb030e65afb
santijll <santijll@example.com>
2025-01-11 18:59:48 +0100

    prueba 9

--- .github/actions/badges/src/index.js
+++ .github/actions/badges/src/index.js
@@ -5,6 +5,11 @@ async function main() {
   try {
     const resultado = core.getInput("resultado");
 
+    const img =
+      resultado === "success"
+        ? "https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg"
+        : "https://img.shields.io/badge/test-failure-red";
+
     const old_readme = await fs.readFile("./OldREADME.md", "utf8");
     const new_readme = old_readme + resultado;
 
